32a7eba35144a04d13093f6a867a9d2476a37432
mankie84 <mankie84@gmx.de>
2015-03-08 16:47:57 -0400

    First commit

---
+++ plot1.R
@@ -0,0 +1,18 @@
+oldwd <- getwd()
+
+if (file.exists("exdata-data-household_power_consumption")){				## check if specdata is there
+		setwd("exdata-data-household_power_consumption")				## set new wd
+}		else {
+		writeLines("Please move the 'exdata-data-household_power_consumption' folder to the current working directory! \nThen run again. Thx")
+		stop()
+}
+
+dat <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors = FALSE, na.strings="?")
+dat$newDate <- as.Date(dat$Date,"%d/%m/%Y")
+dat$date_time <- as.POSIXct(paste(dat$newDate, dat$Time), format="%Y-%m-%d %H:%M:%S")
+mydat <- dat[dat$newDate %in% as.Date(c('2007-02-01', '2007-02-02')),]
+
+png(filename = "plot1.png", width = 480, height = 480, units = "px")
+hist(mydat$Global_active_power, col="red", main="Global Active Power",xlab="Global Active Power (kilowatts)")
+dev.off()
+setwd(oldwd)
