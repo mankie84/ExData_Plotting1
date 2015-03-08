424f047916821d2feb589d7b305012d092d1b0c3
mankie84 <mankie84@gmx.de>
2015-03-08 17:02:56 -0400

    Second Commit

--- plot3.R
+++ plot3.R
@@ -1,17 +1,19 @@
 oldwd <- getwd()
 
-if (file.exists("exdata-data-household_power_consumption")){				## check if specdata is there
-		setwd("exdata-data-household_power_consumption")				## set new wd
+if (file.exists("exdata-data-household_power_consumption")){	## check if data is there
+		setwd("exdata-data-household_power_consumption")	## set new wd
 }		else {
 		writeLines("Please move the 'exdata-data-household_power_consumption' folder to the current working directory! \nThen run again. Thx")
 		stop()
 }
 
+##clean data
 dat <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors = FALSE, na.strings="?")
 dat$newDate <- as.Date(dat$Date,"%d/%m/%Y")
 dat$date_time <- as.POSIXct(paste(dat$newDate, dat$Time), format="%Y-%m-%d %H:%M:%S")
 mydat <- dat[dat$newDate %in% as.Date(c('2007-02-01', '2007-02-02')),]
 
+##initiate screen device, plot data and and shut screen device off
 png(filename = "plot3.png", width = 480, height = 480, units = "px")
 plot(mydat$date_time,mydat$Sub_metering_1,ylab="Energy sub metering", pch="",lines(mydat$date_time, mydat$Sub_metering_1),xlab="")
 lines(mydat$date_time, mydat$Sub_metering_2,col="red")
